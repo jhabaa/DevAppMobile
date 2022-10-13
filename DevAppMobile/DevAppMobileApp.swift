ddc5558c0d6649fb7fe35df6b4f0bc4ff03c2ced
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2022-10-13 21:41:14 +0200

    Initial Commit

---
+++ DevAppMobile/DevAppMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevAppMobileApp.swift
+//  DevAppMobile
+//
+//  Created by Jean Hubert ABA'A on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DevAppMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
